a22f001584c7f95a369529a8633311a9d6b42a8e
Sharif2138 <s.kiviiri@alustudent.com>
2024-10-16 19:28:21 +0200

    bjfvjdvnjcnvj

--- responsive design/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuBtn = document.getElementById("menuBtn");
-    const navbar = document.getElementById("navbar");
-
-    menuBtn.addEventListener("click", () => {
-        navbar.querySelector("ul").classList.toggle("active");
-    });
-});
